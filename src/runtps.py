cfd65e5309974950d479f4ec88bd7272bb7c1d24
Thiago Nunes <gorobaum@gmail.com>
2014-12-01 18:29:08 -0200

    New deformations

--- src/runtps.py
+++ src/runtps.py
@@ -7,8 +7,6 @@ import controlPoints as cp
 import controlPointFactory as cpf
 from scipy import ndimage
 
-start_timeT = time.time()
-
 original = scipy.misc.imread("static.png")
 modified = scipy.misc.imread("moving.png")
 
@@ -25,18 +23,12 @@ staticCPS = cp.ControlPoints(staticCps)
 movingCPS = cp.ControlPoints(movingCps)
 
 tp = tps.TPS(staticCPS, movingCPS)
-start_time = time.time()
 tp.solveLinearEquation()
-print("--- solveLinearEquation took %s seconds ---" % (time.time() - start_time))
 
 newImage = np.ndarray(original.shape, original.dtype)
-interpolationTime = []
 for x in range(original.shape[0]):
 	for y in range(original.shape[1]):
-		start_time = time.time()
 		newPoint = tp.interpolateIn(x,y)
-		timeToInterpolate = time.time() - start_time
-		interpolationTime.append(timeToInterpolate)
 		newX = newPoint[0]
 		newY = newPoint[1]
 		if newX >= original.shape[0]:
@@ -45,11 +37,3 @@ for x in range(original.shape[0]):
 			newY = original.shape[1]-1
 		newImage[x, y] = modified[newX, newY]
 scipy.misc.imsave("resultUniform.png", newImage)
-
-average = 0
-for role in interpolationTime:
-	average = average + role
-average = average / len(interpolationTime)
-
-print "Average interpolation time" , average
-print("--- TPS took %s seconds ---" % (time.time() - start_timeT))
